8e644e38b4832af4bf038d9bea62f79811f0459c
Soraya Alcobet <soraya.alcobet@gmail.com>
2025-10-17 19:30:58 +0000

    Ocultados los apartados de back y front hasta fecha prevista

--- src/pages/search-index.json.js
+++ src/pages/search-index.json.js
@@ -2,12 +2,18 @@ import { getCollection } from 'astro:content';
 
 /** @type {import('astro').APIRoute} */
 export async function GET() {
+  const today = new Date();
+  const releaseBackEnd = new Date('2025-12-14');
+  const releaseFrontEnd = new Date('2026-02-22');
+
   const javascript = await getCollection('javascript');
   const html = await getCollection('html');
   const css = await getCollection('css');
-  const bases_de_datos = await getCollection('bases_de_datos');
-  const nodejs = await getCollection('nodejs');
-  const react = await getCollection('react');
+
+  const bases_de_datos = today >= releaseBackEnd ? await getCollection('bases_de_datos') : [];
+  const nodejs = today >= releaseBackEnd ? await getCollection('nodejs') : [];
+  const react = today >= releaseFrontEnd ? await getCollection('react') : [];
+
 
   // Función que añade el prefijo correcto según la colección
   function mapWithPrefix(items, prefix) {
